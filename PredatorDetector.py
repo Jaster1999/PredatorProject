8e0d6bc1834489a33f601c87cb06fed181200351
Stanley <stanleyj2000@gmail.com>
2021-09-18 15:16:18 +1200

    added sequence playback and past frame averaging

--- PredatorDetector.py
+++ PredatorDetector.py
@@ -2,6 +2,7 @@ import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import time
 
 
 def load_images_from_folder(folder):
@@ -18,24 +19,29 @@ def load_images_from_folder(folder):
 def diff_mask(img1, img2, threshold):
     diff = cv.absdiff(img1, img2)
     mask = diff>threshold
-    return mask
+    mask = mask*255
+    return mask.astype(np.uint8)
 
 def main():
     CWD = os.getcwd()
     folderOfSeq = 'Video sequences for project-20210918'
     seq = 'seq7'
     sequence = load_images_from_folder(os.path.join(CWD, folderOfSeq, seq))
-    mean_frame = np.mean(sequence[0:10], axis=0).astype(np.uint8)
-    plt.imshow(mean_frame, cmap="gray")
-    imagenumber = 10
-    thresholdValue = 10
-    mask = diff_mask(mean_frame, sequence[imagenumber], thresholdValue)
-    plt.figure()
-    plt.imshow(mask, cmap='gray')
-    plt.figure()
-    plt.imshow(sequence[imagenumber], cmap='gray')
-    plt.show()
     
+    for imagenumber in range(len(sequence)):
+        if imagenumber < 5:
+            mean_frame = np.mean(sequence[0:5], axis=0).astype(np.uint8)
+        else:
+            mean_frame = np.mean(sequence[imagenumber-5:imagenumber], axis=0).astype(np.uint8)
+        thresholdValue = 10
+        mask = diff_mask(mean_frame, sequence[imagenumber], thresholdValue)
+        cv.imshow("mask", mask)
+        cv.waitKey(1)
+        cv.imshow("image", sequence[imagenumber])
+        cv.waitKey(1)
+        time.sleep(0.5)
+    cv.destroyAllWindows()
+
     return
 
 if __name__ == "__main__":
